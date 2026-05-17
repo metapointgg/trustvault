3c3c30642f8b19d49dc3c11730b076b6d5de1b44
Mike O <93396632+metapointgg@users.noreply.github.com>
2026-05-17 23:33:08 +0100

    Add selected customer refresh trigger

--- frontend/trustvault_app/lib/shared/selected_customer.dart
+++ frontend/trustvault_app/lib/shared/selected_customer.dart
@@ -2,6 +2,7 @@ import 'package:flutter/foundation.dart';
 
 class SelectedCustomerController {
   static final ValueNotifier<Map<String, dynamic>?> selected = ValueNotifier<Map<String, dynamic>?>(null);
+  static final ValueNotifier<int> refreshToken = ValueNotifier<int>(0);
 
   static String? get externalId => selected.value?['external_id']?.toString();
 
@@ -15,5 +16,9 @@ class SelectedCustomerController {
 
   static void select(Map<String, dynamic>? customer) {
     selected.value = customer;
+  }
+
+  static void requestRefresh() {
+    refreshToken.value = refreshToken.value + 1;
   }
 }
